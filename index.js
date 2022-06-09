715676f3f6ef3d06103f209922895638a9a2eef8
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-09 11:19:27 +0300

    commit tree 876c29b717a2bc9218d1832df3c87b402ec5412a

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31, num2 = 2, num3=10, num4=6;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10)+1;
+
+let mod = num3 % num4;
+
+let max = Math.max(4, 5, 2, 20); 
